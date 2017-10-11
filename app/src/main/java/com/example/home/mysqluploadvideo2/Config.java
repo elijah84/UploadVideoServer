1e2259a1149af882638ed6521d4eb0e048e797b5
elijah84 <assefa.belaye@gmail.com>
2017-10-11 16:56:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/home/mysqluploadvideo2/Config.java
@@ -0,0 +1,11 @@
+package com.example.home.mysqluploadvideo2;
+
+/**
+ * Created by Belal on 11/23/2015.
+ */
+public class Config {
+    public static final String UPLOAD_URL= "http://r3mm1k5.net/VideoUpload/upload.php";
+
+    public static final String TAG_URL = "url";
+    public static final String TAG_SIZE = "size";
+}
